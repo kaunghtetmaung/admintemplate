0a767c4668b5cb5ed089c79e802950116db8a5c8
Kaung Htet Maung <kaunghtetmaung260@gmail.com>
2024-07-04 14:23:19 +0630

    my project

---
+++ js/app.js
@@ -0,0 +1,25 @@
+    // Start Navbar 
+       // start top navbar
+           function dropbtn(e){
+                // console.log(e.target);
+
+                // console.log(e.target.parentElement.nextElementSibling);
+
+                e.target.parentElement.nextElementSibling.classList.toggle("show");
+           }
+           
+
+       // end top navbar
+    // End Navbar
+
+$(document).ready(function(){
+    // Start Navbar
+        // start left sidebar
+            $(".sidebarlinks").click(function(){
+                $(".sidebarlinks").removeClass("currents");
+                $(this).addClass("currents");
+            });
+        // end left sidebar
+    // End Navbar
+});
+// 21DD 
